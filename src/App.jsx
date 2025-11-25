df072a6a5cc8c5cec9220b371f9896caa5bc76c2
ndulue098 <christianndulue38@gmail.com>
2025-11-25 10:51:11 +0100

    implement cocktail section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {gsap} from "gsap";
 import {ScrollTrigger,SplitText} from "gsap/all";
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
+import Cocktail from "./components/Cocktail.jsx";
 
 gsap.registerPlugin(ScrollTrigger,SplitText);
 
@@ -10,8 +11,7 @@ export default function App(){
         <main>
             <Navbar/>
             <Hero/>
-            <div className={"h-dvh bg-amber-300"}>
-            </div>
+            <Cocktail/>
         </main>
     );
 }
